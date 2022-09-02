cdd6d148de3b234e623ff66f07d525ee64bb2432
pulsarforge <omgworkshopfire@gmail.com>
2022-09-02 22:09:48 +0200

    Update Description

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -3,18 +3,25 @@ import { Typography } from '@material-ui/core';
 
 const Description = () => (
   <>
+    <Typography variant="h5">
+      What is Awesome Web3 Radar?
+    </Typography>
     <Typography variant="h6">
-      What is the Tech Radar?
+      It is an exercise to discover web3 space and sharing.
     </Typography>
 
     <Typography variant="subtitle2">
       This tool Based on the <a href="https://www.thoughtworks.com/radar">pioneering
       work of ThoughtWorks</a> and <a href="https://github.com/zalando/tech-radar">Zalando&#39;s project</a>.
-      yaneek's tech radar <a href="https://github.com/yaneek/tech-radar">yaneek project</a>
+      fork basesd on yaneek's tech radar <a href="https://github.com/yaneek/tech-radar">yaneek project</a>.
+    </Typography>
+    <Typography variant="subtitle2">
+      Awesome Web3 is developed and enhanced by <a href="https://github.com/pulsarforge">pulsarforge</a>.
+      Contact me at discord community <a href="https://discord.com/invite/frYAsD2DjT">Blocks / Voxelverse Labs</a>.
     </Typography>
 
     <Typography align="center">
-      &copy; 2022 Inursites
+      &copy; 2022-2023 Inursites - pulsarforge
     </Typography>
   </>
 )
